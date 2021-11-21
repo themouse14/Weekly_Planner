1d91663da0abc1e9dfbc50e24d3e515209dd5d10
themouse14 <themouse.ow@gmail.com>
2021-11-20 17:51:30 -0700

    tried adding new fucntions, did not behave as expected

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
-// today's date inside header
+// today's date inside header, initialize local storage at load
 
-const today = moment();
-$("#currentDay").text(today.format("MMM Do YY"));
+function init() {
+    const today = moment();
+    $("#currentDay").text(today.format("MMM Do YY"));
 
+    colorTimeBlocks();
+    setInterval(colorTimeBlocks, 60000);
+    $(".time-block").each(function() {
+        var blockId = $(this).attr("id");
+        // load saved data from local storage
+        $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+    });
+    
+    $(".saveBtn").on("click", handleSave);
+};
 // variables
 
 //const mondayEl = $('<div>');
@@ -42,13 +53,35 @@ for (var i = 0; i < timeSlot.length; i++) {
     
     taskEl.addClass('col-md-10');
     completedEl.addClass('col-md-1');
-    timeSlotEl.addClass('row');
+    timeSlotEl.addClass('row', 'hour');
     timeSlotEl.append(taskEl);
     timeSlotEl.append(completedEl);
     plannerContainer.append(timeSlotEl);
 }
 
-// setting attributes to timeSlot slots
+function colorTimeBlocks() {
+    $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    
+    $(this).removeClass("past present future");
+    
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    } else if (blockHour > currentHour) {
+        $(this).addClass("future");
+    } else {
+        $(this).addClass("present");
+    }
+    });
+}
+
+// function for saving
+
+function handleSave(event) {
+    var hourId = $(this).parent().attr("id");
+    localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
+}
 
 
 
